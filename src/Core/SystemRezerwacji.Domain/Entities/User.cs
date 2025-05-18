bee9bac6cfe1246a17bd5b482b93e09736896c66
Patryk Przydanek <przydan1@gmail.com>
2025-05-19 00:10:07 +0200

    - jw

---
+++ src/Core/SystemRezerwacji.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace SystemRezerwacji.Domain.Entities;
+
+public class User : IdentityUser<Guid>
+{
+    public Guid Id { get; set; } // Później może być dziedziczone z IdentityUser<Guid>
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Email { get; set; } // Ważne dla identyfikacji
+
+    // Nawigacja do rezerwacji
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+}
