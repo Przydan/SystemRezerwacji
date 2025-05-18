5ada7ac58cf56241b3d9e775f31c02de6d279d09
leonidassq <leonstolecki@outlook.com>
2025-05-18 15:34:25 +0200

    Initial project structure and resolved Git issues

---
+++ src/Presentation/SystemRezerwacji.ClientApp/Models/ResourceDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SystemRezerwacji.ClientApp.Models
+{
+    // SystemRezerwacji.ClientApp/Models/ResourceDto.cs
+    using System;
+
+    public class ResourceDto
+    {
+        public Guid Id { get; set; } // Zakładając, że ID jest GUIDem
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        // Możesz dodać więcej pól, jeśli API je zwraca i chcesz je od razu wyświetlić,
+        // np. Location, Capacity, IsActive, ResourceTypeName
+        // Na razie, zgodnie z wymaganiem MVP, wystarczą Name i Description.
+    }
+}
