e4e014aa0c22d1470dc987a6b59768c539ab475f
przydan <przydan@hotmail.com>
2025-05-19 00:10:07 +0200

    - jw

---
+++ src/Core/SystemRezerwacji.Domain/Entities/Booking.cs
@@ -0,0 +1,23 @@
+using SystemRezerwacji.Domain.Enums;
+
+namespace SystemRezerwacji.Domain.Entities;
+
+public class Booking
+{
+    public Guid Id { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public DateTime BookedAt { get; set; } = DateTime.UtcNow; // Domyślnie czas utworzenia
+    public DateTime? LastModifiedAt { get; set; } // Opcjonalne
+    public string? Notes { get; set; }
+    
+    public BookingStatus Status { get; set; } // Enum, który zdefiniujemy
+
+    // Klucz obcy i nawigacja do Resource
+    public Guid ResourceId { get; set; }
+    public Resource Resource { get; set; }
+
+    // Klucz obcy i nawigacja do User
+    public Guid UserId { get; set; } // Na razie Guid, później połączymy z IdentityUser
+    public User User { get; set; } // Właściwość nawigacyjna do encji User, którą dodamy
+}
---
+++ src/Core/SystemRezerwacji.Domain/Entities/ResourceType.cs
@@ -0,0 +1,11 @@
+namespace SystemRezerwacji.Domain.Entities;
+
+public class ResourceType
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string? IconCssClass { get; set; } // Opcjonalne
+    
+    public ICollection<Resource> Resources { get; set; } = new List<Resource>();
+}
