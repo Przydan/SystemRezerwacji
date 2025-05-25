c2a3d77123d1ed68c542656d66920a57e0a9de70
Kacper1909 <kacper.dombrowicz@icloud.com>
2025-05-25 13:43:47 +0200

    KD - dodałem/zmodyfikowałem  pliki: _Imports.razor Program.cs SystemRezerwacji.ClientApp.csproj ResourceForm.razor ResourceDto.cs ManageResources.razor ResourceClientService.cs MainLayout.razor

---
+++ src/Presentation/SystemRezerwacji.ClientApp/Models/ResourceTypeDto.cs
@@ -0,0 +1,8 @@
+namespace SystemRezerwacji.ClientApp.Models
+{
+    public class ResourceTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
