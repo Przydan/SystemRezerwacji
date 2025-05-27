d24e00869e3e9a2ff1b95b0a7598d3c897276c71
Kacper1909 <kacper.dombrowicz@icloud.com>
2025-05-27 21:52:18 +0200

    KD

--- src/Infrastructure/SystemRezerwacji.Infrastructure/Migrations/20250527192419_init.Designer.cs
+++ src/Infrastructure/SystemRezerwacji.Infrastructure/Migrations/20250527192419_init.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SystemRezerwacji.Infrastructure.Persistence.DbContext;
 
@@ -10,9 +11,11 @@ using SystemRezerwacji.Infrastructure.Persistence.DbContext;
 namespace SystemRezerwacji.Infrastructure.Migrations
 {
     [DbContext(typeof(SystemRezerwacjiDbContext))]
-    partial class SystemRezerwacjiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250527192419_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.16");
