739e4d5f5336c4492f5dec6a1495bff0dc5b5cd7
przydan <przydan@hotmail.com>
2025-06-07 06:04:12 +0200

    -- update Resource logic

--- src/Shared/DTOs/Resource/UpdateResourceRequestDto.cs
+++ src/Shared/DTOs/Resource/UpdateResourceRequestDto.cs
@@ -2,4 +2,6 @@ namespace Shared.DTOs.Resource;
 
 public class UpdateResourceRequestDto
 {
+    public Guid Id { get; set; } // Musi być Guid
+    public string Name { get; set; }
 }
--- src/Shared/DTOs/Resource/UpdateResourceTypeRequestDto.cs
+++ src/Shared/DTOs/Resource/UpdateResourceTypeRequestDto.cs
@@ -2,4 +2,6 @@ namespace Shared.DTOs.Resource;
 
 public class UpdateResourceTypeRequestDto
 {
+    public int Guid { get; set; }
+    public string Name { get; set; }
 }
