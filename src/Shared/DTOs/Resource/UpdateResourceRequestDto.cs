29b248c8387d9fc847589b2985855b25b221eff2
przydan <przydan@hotmail.com>
2025-07-15 23:05:39 +0200

    - eliminacja błędów

--- src/Shared/DTOs/Resource/UpdateResourceRequestDto.cs
+++ src/Shared/DTOs/Resource/UpdateResourceRequestDto.cs
@@ -4,4 +4,8 @@ public class UpdateResourceRequestDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public string? Location { get; set; }
+    public int Capacity { get; set; }
+    public int ResourceTypeId { get; set; }
 }
