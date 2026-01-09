eab919b8a04ae663164cdf4dc6b2462eec8ad8de
Patryk Przydanek <przydan@example.com>
2026-01-09 21:05:15 +0100

    fix: Add AccessDenied action and view

--- src/Presentation/Server/Controllers/AccountController.cs
+++ src/Presentation/Server/Controllers/AccountController.cs
@@ -69,4 +69,10 @@ public class AccountController : Controller
         await _signInManager.SignOutAsync();
         return RedirectToAction("Index", "Home");
     }
+
+    [HttpGet]
+    public IActionResult AccessDenied()
+    {
+        return View();
+    }
 }
